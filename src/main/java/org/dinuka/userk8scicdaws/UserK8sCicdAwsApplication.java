3a54db33308964784cdd0f73249d9065cfb2293d
Dinuka Ekanayake <dinukaekanayake92@gmail.com>
2024-06-15 20:31:45 +0800

    init user  ms

---
+++ src/main/java/org/dinuka/userk8scicdaws/UserK8sCicdAwsApplication.java
@@ -0,0 +1,13 @@
+package org.dinuka.userk8scicdaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserK8sCicdAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserK8sCicdAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dinuka/userk8scicdaws/UserK8sCicdAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dinuka.userk8scicdaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserK8sCicdAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
